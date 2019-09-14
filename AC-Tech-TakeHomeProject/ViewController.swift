6ff988e1eef011d8d3e58d645ef4b76fd8d06c1d
Alexei Sitalov <alexei.sitalov@endava.com>
2019-09-14 14:09:09 +0300

    Initial Commit

---
+++ AC-Tech-TakeHomeProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AC-Tech-TakeHomeProject
+//
+//  Created by Alexei Sitalov on 9/14/19.
+//  Copyright © 2019 Alexei Sitalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
